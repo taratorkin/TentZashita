ea2ad4a0b129979cb22a392d7f1ad4e2875fd948
unknown <konstantintaratorkin@mail.ru>
2019-11-24 17:55:20 +0300

    added goods

--- src/Content.js
+++ src/Content.js
@@ -38,6 +38,60 @@ function Content() {
 
         </div>
       </div>
+
+      <div className="content_goods_wrapper">
+        <a name="Goods"></a>
+        <div className="content_goods_headline"><h2>Каталог товаров</h2></div>
+        <div className="content_goods">
+          <div className="content_goods_block">
+            <div className="content_goods_block_text">
+              <div className="content_goods_block_text_margin">
+                <h2>Тенты на газель и газель next</h2>
+                <p>Цена: от 6750 р.</p>
+              </div>
+            </div>
+            <div className="content_goods_block_image content_goods_block_image1"></div>
+          </div>
+          <div className="content_goods_block">
+            <div className="content_goods_block_image content_goods_block_image2"></div>
+            <div className="content_goods_block_text">
+              <div className="content_goods_block_text_margin">
+                <h2>Тенты-чехлы для автомобилей</h2>
+                <p>Цена: от 3750 р.</p>
+              </div>
+            </div>
+          </div>
+          <div className="content_goods_block">
+            <div className="content_goods_block_text">
+              <div className="content_goods_block_text_margin">
+                <h2>Тенты на прицепы для автомобилей</h2>
+                <p>Материал: ПВХ, брезент</p>
+                <p>Цена: от 3750 р.</p>
+              </div>
+            </div>
+            <div className="content_goods_block_image content_goods_block_image3"></div>
+          </div>
+          <div className="content_goods_block">
+            <div className="content_goods_block_image content_goods_block_image4"></div>
+            <div className="content_goods_block_text">
+              <div className="content_goods_block_text_margin">
+                <h2>Автопокрывала для грузовых автомобилей</h2>
+                <p>Материал: ПВХ, брезент</p>
+                <p>Цена: уточнить у менеджера</p>
+              </div>
+            </div>
+          </div>
+          <div className="content_goods_block">
+            <div className="content_goods_block_text">
+              <div className="content_goods_block_text_margin">
+                <h2>Термочехлы из ПВХ для перевозки грузов на поддонах.</h2>
+                <p>Цена: В зависимости от размеров чехла и вида материала</p>
+              </div>
+            </div>
+            <div className="content_goods_block_image content_goods_block_image5"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
