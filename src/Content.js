c816500ad4d3c2f9f236c19ef7e94f7681737d6e
unknown <konstantintaratorkin@mail.ru>
2019-11-24 16:51:11 +0300

    added aboutUs

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './css/content.css';
 import tentImg from './images/tent.jpg';
+import tentImg2 from './images/tent2.jpg';
 
 function Content() {
   return(
     <div className="content">
+
       <div className="content_mainScreen_wrapper">
         <div className="content_mainScreen">
           <div className="content_mainScreen_block">
@@ -15,8 +17,25 @@ function Content() {
             <p className="content_mainScreen_contacts">+7 (960) 299-92-29</p>
           </div>
           <div className="content_mainScreen_block">
-            <img className="content_mainScreen_image" src={tentImg} alt=""/>
+            <img className="content_mainScreen_image" src={tentImg} alt="Палатка сварщика"/>
           </div>
+        </div>
+      </div>
+
+      <div className="content_about">
+        <a name="AboutUs"></a>
+        <div className="content_about_element">
+          <div className="content_about_element_text">
+            <h2>
+              Мы предлагаем
+            </h2>
+            Пошив и ремонт тентов. Собственное производство. Доставка во все регионы.
+            Изготовление по индивидуальным заказам. Принимаем оплату, в том числе,
+            по безналичному расчёту. Скидки на оптовые заказы.
+          </div>
+        </div>
+        <div className="content_about_image">
+
         </div>
       </div>
     </div>
